e1e131d225e3b3c465f015ec38e5703b70e89df3
David Maene <davidmened@gmail.com>
2022-04-04 12:31:53 +0300

    up me line

---
+++ controllers/player.controller.js
@@ -0,0 +1,12 @@
+const { Player } = require('../models/player.model.js');
+const PlayerController = {
+    login: async (req, res, next) => {
+
+    },
+    register: async (req, res, next) => {
+
+    }
+}
+module.exports = {
+    PlayerController
+}
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { players } = require("./players.routes.js");
 const { admin } = require("./admin.routes.js");
 const routes = express.Router();
 
-routes.use('/webhook/players', players )
+routes.use('/webhook/player', players )
 routes.use('/admin', admin )
 
 module.exports = { routes }
