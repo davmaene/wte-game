4b8db7a9a8d161fb7f98926d7a15cc408586e7b1
David Maene <davidmened@gmail.com>
2022-04-04 12:36:06 +0300

    up me line

--- controllers/player.controller.js
+++ controllers/player.controller.js
@@ -1,10 +1,18 @@
+const { Response } = require('../helpers/helper.message.server');
 const { Player } = require('../models/player.model.js');
 const PlayerController = {
     login: async (req, res, next) => {
-
+        return Response(res, 200, req.body)
     },
-    register: async (req, res, next) => {
-
+    webhook: async (req, res, next) => { // tous les sms atterissent ici
+        const {
+            content,
+            id,
+            idservice,
+            from_number,
+            to_number
+        } = req.body;
+        return Response(res, 200, req.body)
     }
 }
 module.exports = {
--- routes/players.routes.js
+++ routes/players.routes.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const { PlayerController } = require("../controllers/player.controller");
 const players = express.Router();
 
-// players.get("/player/login",)
+players.get("/login", PlayerController.login)
+players.get("/", PlayerController.login)
 
 module.exports = { players }
