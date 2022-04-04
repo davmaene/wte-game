a7f5eb786d0c61187f348ccf346ea5fe95e38110
David Maene <davidmened@gmail.com>
2022-04-04 12:24:17 +0300

    up me line

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const { players } = require("./players.routes.js");
 const { admin } = require("./admin.routes.js");
 const routes = express.Router();
 
-routes.use('/players', players )
+routes.use('/webhook/players', players )
 routes.use('/admin', admin )
 
 module.exports = { routes }
