fb04f4b356568817e52ef5159cd360227e084ec1
David Maene <davidmened@gmail.com>
2022-04-04 16:33:19 +0300

    up me line

--- controllers/player.controller.js
+++ controllers/player.controller.js
@@ -56,7 +56,7 @@ const PlayerController = {
                                 // this means the player already exist
                                 sendMessage({
                                     to: fillphone(from_number),
-                                    content: `Les message d'inscription doit contenir 3 parties séparées par des virgules \nex: nom prenom, province, ville`
+                                    content: `nous avons detecter que le numero ${fillphone(from_number)} est déjà utiliser par un autre joueur !`
                                 }, (er, dn) => {
                                     if(er){
                                         logger({message: "erreur on sending message", raison: er});
