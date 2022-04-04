f813db97a628b6b4ec66c1947018f9381ff8d766
David Maene <davidmened@gmail.com>
2022-04-04 12:37:03 +0300

    up me line

--- routes/players.routes.js
+++ routes/players.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { PlayerController } = require("../controllers/player.controller");
 const players = express.Router();
 
+players.post("/", PlayerController.webhook)
 players.get("/login", PlayerController.login)
-players.get("/", PlayerController.login)
 
 module.exports = { players }
