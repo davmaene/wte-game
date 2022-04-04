68c5d960d7bee4e79a155336a20364171248abb8
David Maene <davidmened@gmail.com>
2022-04-04 16:23:19 +0300

    up me line

--- helpers/helpers.message.js
+++ helpers/helpers.message.js
@@ -9,7 +9,7 @@ const project = tr.initProjectById(process.env.APP_PROJECTID);
 const sendMessage = async ({ to, content }, cb ) => {
     await project.sendMessage({
         to_number: to, 
-        content,
+        content: `#${process.env.APP_NAME}\n${content}`,
     }, (err, message) => {
         if (err) cb(err, undefined)
         else cb(undefined, message)
