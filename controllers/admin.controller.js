bd67229925183b1b48976b8430a78d8166cb93f3
David Maene <davidmened@gmail.com>
2022-06-20 14:37:03 +0200

    up me line

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,13 +1,29 @@
 const { Response } = require('../helpers/helper.message.server');
 const { fillphone } = require("../helpers/helper.fillphonenumber");
-const { Player } = require("../models/player.model");
+const { Admin } = require("../models/admin.model");
 
 const AdminController = {
     signup: async (req, res, next) => {
-        const { fsname, lsname, phone, email } = req.body;
-        if(!fsname || !lsname || !phone || !email) return Response(res, 401, "this request must have body included fsname, lsname, phone, email")
+        const { fsname, lsname, phone, email, password } = req.body;
+        if(!fsname || !lsname || !phone || !email || !password) return Response(res, 401, "this request must have body included fsname, lsname, phone, email")
         try {
-            
+            Admin.create({
+                fsname,
+                lsname,
+                phone: fillphone(phone),
+                email,
+                ref: "",
+                accesslevel: 1,
+
+            })
+            .then(adm => {
+                if(adm instanceof Admin){
+
+                }else Response(res, 400, adm)
+            })
+            .catch(err => {
+                return Response(res, 500, err)
+            })
         } catch (error) {
             return Response(res, 500, error)
         }
--- helpers/helper.random.js
+++ helpers/helper.random.js
@@ -1,27 +1,17 @@
 const randomstring = require("randomstring");
+const bcrypt = require("bcrypt");
 const dotenv = require('dotenv');
 
 dotenv.config();
 
 const generateIdentifier = () => {
     const pfx = Math.floor(Math.random() * 1000);
-    const sfx = Math.floor(Math.random() * 100);
+    const sfx = `${Math.floor(Math.random() * 100)}${Math.floor(Math.random() * 100)}`;
     
     return `WTE-${pfx}-${sfx}`;
 
-}
-const randomVoucherRecharge = () => {
-    const len = 18;
-    const ret = [];
+};
 
-    for(let k = 0; k < len; k++) ret.push(
-       Math.floor( Math.random() * 10 )
-    );
-    
-    let m = ret.join().toString();
-    m = m.replace(/,/g, "");
-    return m;
-}
 const randomVerifierAccount = () => {
     const len = 6;
     const ret = [];
@@ -33,17 +23,30 @@ const randomVerifierAccount = () => {
     let m = ret.join().toString();
     m = m.replace(/,/g, "");
     return m;
-}
+};
+
 const genFilename = async (length = new Number()) => {
     return randomstring.generate({
         length: 17,
         charset: process.env.app_accesskey
     })
-}
+};
+
+const generatePasswordAndEncryptIt = async (roundsalt) => {
+
+    const sfx = `${Math.floor(Math.random() * 100)}${Math.floor(Math.random() * 100)}`;
+    const salt = await bcrypt.genSalt(roundsalt ? roundsalt : 10);
+    const hashed = await bcrypt.hash(sfx, salt);
+
+    return {
+        plain: sfx,
+        encrypted: hashed
+    }
+};
 
 module.exports = {
+    generatePasswordAndEncryptIt,
     generateIdentifier,
     genFilename,
-    randomVerifierAccount,
-    randomVoucherRecharge
+    randomVerifierAccount
 }
